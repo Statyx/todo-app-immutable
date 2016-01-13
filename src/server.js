74d2c69d5c3427f673daaf1dcd3b3a372c59333a
Julien Meichelbeck <julien.meichelbeck@gmail.com>
2016-01-13 16:27:09 -0700

    Transfer state through sockets

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export function startServer(store) {
   const io = new Server().attach(8090);
+
+  // This is really heavy, should be refactored later
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store)); // Security threat to accept everything
+  });
 }
